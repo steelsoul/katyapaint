4d9ce893aa7f2f6fce8e1cc2ff78d7488457ad75
Alex Pushkar <alexanderpushkar86@gmail.com>
2017-01-29 01:05:08 +0200

    Add paint tools. Bitmap has correct size.

--- app/src/main/java/org/athome/alex/katyapaint/MainActivity.java
+++ app/src/main/java/org/athome/alex/katyapaint/MainActivity.java
@@ -7,33 +7,62 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
-import android.support.v7.app.ActionBar;
+import android.graphics.Rect;
+
+import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = "[KP:Main]";
     DrawView drawView;
+    PaintTools paintTools;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "ctor");
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
         LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         setContentView(linearLayout, linearLayoutParams);
 
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        paintTools = new PaintTools(this);
+        paintTools.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 3f));
+        paintTools.setBackgroundResource(R.drawable.back);
+        linearLayout.addView(paintTools);
+
         drawView = new DrawView(this);
-        drawView.setLayoutParams(layoutParams);
+        drawView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+        drawView.setBackgroundResource(R.drawable.back);
         linearLayout.addView(drawView);
+    }
+
+    class PaintTools extends  View {
+        final String LOG_TAG = "[KP:PaintTools]";
+
+        public PaintTools(Context context) {
+            super(context);
+            Log.i(LOG_TAG, "ctor");
+        }
     }
 
     class DrawView extends View implements View.OnTouchListener {
@@ -42,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
         Path path;
         Canvas canvas;
         Bitmap bitmap;
+        Rect dest;
+
 
         public DrawView(Context context) {
             super(context);
@@ -54,10 +85,19 @@ public class MainActivity extends AppCompatActivity {
             Display display = getWindowManager().getDefaultDisplay();
             Point size = new Point();
             display.getSize(size);
-            bitmap = Bitmap.createBitmap(size.x, size.y, Bitmap.Config.RGB_565);
-            canvas = new Canvas(bitmap);
-
+            bitmap = null;
+            canvas = null;
+            dest = new Rect();
             setOnTouchListener(this);
+        }
+
+        @Override
+        public void onSizeChanged(int w, int h, int oldw, int oldh) {
+            Log.d(TAG, "onSizeChanged: " + w + ", " + h);
+            bitmap = Bitmap.createBitmap(w-8, h-8, Bitmap.Config.RGB_565);
+            canvas = new Canvas(bitmap);
+            canvas.drawARGB(255, 255, 255, 255);
+            dest.set(4, 4, w-4, h-4);
         }
 
         @Override
@@ -84,8 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onDraw(Canvas canvas) {
-            canvas.drawBitmap(bitmap, 0, 0, paint);
+            if (bitmap != null) canvas.drawBitmap(bitmap, null, dest, paint);
         }
-
     }
 }
