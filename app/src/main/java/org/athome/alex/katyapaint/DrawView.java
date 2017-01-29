fb8f8b4880107d6b05de42cc1e7b5e89b6d879b6
Alex Pushkar <alexanderpushkar86@gmail.com>
2017-01-29 02:01:27 +0200

    Using XML layouts.

---
+++ app/src/main/java/org/athome/alex/katyapaint/DrawView.java
@@ -0,0 +1,80 @@
+package org.athome.alex.katyapaint;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Provides paint field able to react on user input
+ * @author Alexander Pushkar
+ * @version 1.0
+ * @since 2016-01-29
+ */
+
+class DrawView extends View implements View.OnTouchListener {
+    final String LOG_TAG = "[KP:DrawView]";
+    Paint paint;
+    Path path;
+    Canvas canvas;
+    Bitmap bitmap;
+    Rect dest;
+
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Log.i(LOG_TAG, "ctor");
+        paint = new Paint();
+        paint.setStrokeWidth(3);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLUE);
+        path = new Path();
+        bitmap = null;
+        canvas = null;
+        dest = new Rect();
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public void onSizeChanged(int w, int h, int oldw, int oldh) {
+        Log.d(LOG_TAG, "onSizeChanged: " + w + ", " + h);
+        bitmap = Bitmap.createBitmap(w-8, h-8, Bitmap.Config.RGB_565);
+        canvas = new Canvas(bitmap);
+        canvas.drawARGB(255, 255, 255, 255);
+        dest.set(4, 4, w-4, h-4);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.i(LOG_TAG, "onTouch ACTION_DOWN");
+            path.reset();
+            path.moveTo(event.getX(), event.getY());
+            return true;
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            path.lineTo(event.getX(), event.getY());
+            canvas.drawPath(path, paint);
+            invalidate();
+            return true;
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            Log.i(LOG_TAG, "onTouch ACTION_UP");
+            canvas.drawPath(path, paint);
+            invalidate();
+            return true;
+        }
+        Log.d(LOG_TAG, "onTouch event fired action: " + MotionEvent.actionToString(event.getAction()));
+        return false;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (bitmap != null) canvas.drawBitmap(bitmap, null, dest, paint);
+    }
+}
--- app/src/main/java/org/athome/alex/katyapaint/MainActivity.java
+++ app/src/main/java/org/athome/alex/katyapaint/MainActivity.java
@@ -1,38 +1,21 @@
 package org.athome.alex.katyapaint;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Point;
-import android.graphics.Rect;
-
-import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String TAG = "[KP:Main]";
+    final String LOG_TAG = "[KP:Main]";
     DrawView drawView;
-    PaintTools paintTools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "ctor");
+        Log.i(LOG_TAG, "ctor");
+
+        setContentView(R.layout.activity_main);
 
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
@@ -40,91 +23,6 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        setContentView(linearLayout, linearLayoutParams);
-
-        paintTools = new PaintTools(this);
-        paintTools.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 3f));
-        paintTools.setBackgroundResource(R.drawable.back);
-        linearLayout.addView(paintTools);
-
-        drawView = new DrawView(this);
-        drawView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
-        drawView.setBackgroundResource(R.drawable.back);
-        linearLayout.addView(drawView);
-    }
-
-    class PaintTools extends  View {
-        final String LOG_TAG = "[KP:PaintTools]";
-
-        public PaintTools(Context context) {
-            super(context);
-            Log.i(LOG_TAG, "ctor");
-        }
-    }
-
-    class DrawView extends View implements View.OnTouchListener {
-        final String TAG = "[KP:DrawView]";
-        Paint paint;
-        Path path;
-        Canvas canvas;
-        Bitmap bitmap;
-        Rect dest;
-
-
-        public DrawView(Context context) {
-            super(context);
-            Log.i(TAG, "ctor");
-            paint = new Paint();
-            paint.setStrokeWidth(3);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.BLUE);
-            path = new Path();
-            Display display = getWindowManager().getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            bitmap = null;
-            canvas = null;
-            dest = new Rect();
-            setOnTouchListener(this);
-        }
-
-        @Override
-        public void onSizeChanged(int w, int h, int oldw, int oldh) {
-            Log.d(TAG, "onSizeChanged: " + w + ", " + h);
-            bitmap = Bitmap.createBitmap(w-8, h-8, Bitmap.Config.RGB_565);
-            canvas = new Canvas(bitmap);
-            canvas.drawARGB(255, 255, 255, 255);
-            dest.set(4, 4, w-4, h-4);
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                Log.i(TAG, "onTouch ACTION_DOWN");
-                path.reset();
-                path.moveTo(event.getX(), event.getY());
-                return true;
-            } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                path.lineTo(event.getX(), event.getY());
-                canvas.drawPath(path, paint);
-                invalidate();
-                return true;
-            } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                Log.i(TAG, "onTouch ACTION_UP");
-                canvas.drawPath(path, paint);
-                invalidate();
-                return true;
-            }
-            Log.d(TAG, "onTouch event fired action: " + MotionEvent.actionToString(event.getAction()));
-            return false;
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            if (bitmap != null) canvas.drawBitmap(bitmap, null, dest, paint);
-        }
+        drawView = (DrawView) findViewById(R.id.drawView);
     }
 }
